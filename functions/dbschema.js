d16a284b671604b0ec07045ef9e99101337f397f
Slinkter <luis.j.cueva@gmail.com>
2020-02-03 15:07:23 -0500

    42:42 : Registro de usuario

---
+++ functions/dbschema.js
@@ -0,0 +1,12 @@
+let db  = {
+    screams : [
+        {
+            userHandle : 'user',
+            body : 'this is the scream body',
+            createdAt : '2020-02-03T19:37:34.306Z',
+            likeCount : 5,
+            commentCount : 2 
+
+        }
+    ]
+}
