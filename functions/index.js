92b607281b4c2256f6ca9ec005f458ccfa2698e9
Slinkter <luis.j.cueva@gmail.com>
2020-02-03 16:25:24 -0500

    no

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,23 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 //
-const express = require("express");
-const app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyApCLCOdgwcdpvMQhEdeczgaXm9IHIBZBI",
+  authDomain: "webproject-f896a.firebaseapp.com",
+  databaseURL: "https://webproject-f896a.firebaseio.com",
+  projectId: "webproject-f896a",
+  storageBucket: "webproject-f896a.appspot.com",
+  messagingSenderId: "1034456639516"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
 //
 app.get("/screams", (req, res) => {
   admin
     .firestore()
     .collection("screams")
-    .orderBy('createAt','desc')
+    .orderBy("createAt", "desc")
     .get()
     .then(data => {
       let screams = [];
@@ -46,4 +55,26 @@ app.post("/scream", (req, res) => {
     });
 });
 
-exports.api = functions.region('us-east1').https.onRequest(app);
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+});
+
+firebase
+  .auth()
+  .createUserWithEmailAndPassword(newUser.email, newUser.password)
+  .then(data => {
+    return res
+      .status(201)
+      .json({ message: ` user ${data.user.uid} signed up sucessfully ` });
+  })
+  .catch(err => {
+    console.error(err);
+    return res.status(500).json({ error: err.code });
+  });
+
+exports.api = functions.region("us-east1").https.onRequest(app);
