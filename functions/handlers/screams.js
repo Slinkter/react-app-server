71d41b66d2f2d24263926c00ece5d91a6895ca5f
Slinkter <luis.j.cueva@gmail.com>
2020-02-10 15:43:39 -0500

    part10:Fin . no funciona por credenciales

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -23,9 +23,10 @@ exports.getAllScreams = (req, res) => {
 };
 
 exports.postOneScream = (req, res) => {
-  
   if (req.body.body.trim() === "") {
-    return res.status(400).json({ body: "body must not be empty" });
+    return res.status(400).json({
+      body: "body must not be empty"
+    });
   }
 
   const newScream = {
@@ -37,10 +38,96 @@ exports.postOneScream = (req, res) => {
   db.collection("screams")
     .add(newScream)
     .then(doc => {
-      res.json({ message: `document ${doc.id} created successfully` });
+      res.json({
+        message: `document ${doc.id} created successfully`
+      });
     })
     .catch(err => {
-      res.status(500).json({ error: "somethine went wrong " });
+      res.status(500).json({
+        error: "somethine went wrong "
+      });
       console.error(err);
+    });
+};
+
+exports.getScream = (req, res) => {
+  let screamData = {};
+
+  db.doc(`/screams/${req.params.screamId}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log("!doc.e");
+        return res.status(400).json({
+          error: "Collection screams : Scream not found"
+        });
+      }
+
+      screamData = doc.data();
+      screamData.screamId = doc.id;
+      // obtenemos el id del documento scream
+      // y nos vamos a la collection comments
+      // y con el id del docuemtnos scream
+      // obtenemos todos los comentarios por el screamId
+      return db
+        .collection("comments")
+        .orderBy("createdAt", "desc") // se crear un index en firebase
+        .where("screamId", "==", req.params.screamId)
+        .get();
+    })
+    .then(data => {
+      screamData.comments = [];
+      // almacenarlos los comentarios en un  arreglos
+
+      data.forEach(doc => {
+        screamData.comments.push(doc.data());
+      });
+      // almacenar el array en un json
+
+      return res.json(screamData);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: err.code
+      });
+    });
+};
+
+//
+exports.commentOnScream = (req, res) => {
+  //
+  if (req.body.body.trim() === "") {
+    return res.status(400).json({
+      error: "must not be empty"
+    });
+  }
+  //
+  const newComment = {
+    body: req.body.body,
+    createdAt: new Date().toISOString(),
+    screamId: req.params.screamId,
+    userHandle: req.user.handle,
+    userImage: req.user.imageUrl
+  };
+
+  db.doc(`/screams/${req.params.screamId}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(404).json({
+          error: "Collecion Screams :  doc scream not found"
+        });
+      }
+      return db.collection("comments").add(newComment);
+    })
+    .then(() => {
+      res.json(newComment);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: "Somethins went wrong " + err.code
+      });
     });
 };
--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,12 @@ const functions = require("firebase-functions");
 const app = require("express")();
 //
 const FBAuth = require("./util/fbAuth.js");
-const { getAllScreams, postOneScream } = require("./handlers/screams");
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream
+} = require("./handlers/screams");
 const {
   signup,
   login,
@@ -25,4 +30,7 @@ app.post("/user", FBAuth, addUserDetails);
 //=================  ==================
 app.get("/user", FBAuth, getAuthenticatedUser);
 //=================  ==================
+app.get("/scream/:screamId", getScream);
+//=================  ==================
+app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 exports.api = functions.https.onRequest(app);
